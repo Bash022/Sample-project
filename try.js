731cc48225679d15a878ad24b8d92851bd447f16
bash022 <geraldkielsantos@gmail.com>
2019-06-27 17:20:48 +0800

    jhj

---
+++ try.js
@@ -0,0 +1,9 @@
+// const person = {
+//     name: "Obaseki Nosa",
+//     location: "Lagos",
+// }
+
+// window.localStorage.setItem('user', JSON.stringify(person));
+
+// const form = document.querySelector('#task-form');
+
